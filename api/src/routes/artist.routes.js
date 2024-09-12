6d66472dff890b59f3e278a74ba7128a59bacb0a
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-09-11 21:17:10 -0300

    remove: removed unused routes

--- api/src/routes/artist.routes.js
+++ api/src/routes/artist.routes.js
@@ -1,29 +1,11 @@
 import { Router } from 'express'
-import { randomArtist, showSomeArtists, createArtist, deleteArtist } from '../controller/artist.controller.js'
+import { randomArtist } from '../controller/artist.controller.js'
 
 const artistRouter = Router()
 
 artistRouter.get('/artist', (req, res) => {
   const response = randomArtist()
   res.json({ response })
-})
-
-artistRouter.get("/artist/:number", (req,res) => {
-  const { number } = req.params
-  const response = showSomeArtists(number)
-  res.json({ response })
-})
-
-artistRouter.post("/artist", (req,res) => {
-  const { name } = req.body 
-  const newArtist = createArtist(name)
-  res.status(200).json({ newArtist })
-})
-
-artistRouter.delete("/artist/:id", (req,res) => {
-  const { id } = req.params
-  const deletedArtist = deleteArtist(id)
-  res.status(200).json({ deletedArtist })
 })
 
 export { artistRouter }
--- api/src/routes/song.routes.js
+++ api/src/routes/song.routes.js
@@ -1,26 +1,8 @@
 import { Router } from 'express'
-import { randomSong, showSomeSongs, createSong, deleteSong } from '../controller/song.controller.js'
+import { randomSong } from '../controller/song.controller.js'
 
 const songRouter = Router()
 
 songRouter.get("/song", randomSong)
-
-songRouter.get("/songs/:number", (req,res) => {
-  const { number } = req.params
-  const response = showSomeSongs(number)
-  res.json({ response })
-})
-
-songRouter.post("/songs", (req,res) => {
-  const { title, artist, album, genre } = req.body 
-  const newSong = createSong(title, artist, album, genre)
-  res.status(200).json({ newSong })
-})
-
-songRouter.delete("/song/:id", (req,res) => {
-  const { id } = req.params
-  const deletedSong = deleteSong(id)
-  res.status(200).json({ deletedSong })
-})
 
 export { songRouter }
