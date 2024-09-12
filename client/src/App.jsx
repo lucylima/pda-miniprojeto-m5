e19e56377722b23036b8c009c6fcd9a34d7bc6d3
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-09-12 17:59:04 -0300

    feat: implementing header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
+import { Song } from './pages/song/Song.jsx'
 function App() {
   return (
     <>
+      <Song /> 
     </>
   )
 }
---
+++ client/src/pages/song/Song.jsx
@@ -0,0 +1,10 @@
+import { Header } from "../../components/Header/Header.jsx"
+function Song() {
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
+
+export { Song }
