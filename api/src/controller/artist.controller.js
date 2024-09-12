83e7ce0fc61503c38ad104c65717508dbc3c8504
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-09-11 23:31:48 -0300

    feat: post method to add artists to the database, get method under mvc pattern

--- api/src/controller/artist.controller.js
+++ api/src/controller/artist.controller.js
@@ -1,10 +1,24 @@
+import { database } from '../database/database.js'
+
 const randomArtist = (req, res) => {
   try {
-    
   } catch (error) {
     return res.status(400).json({ erro })
   }
-};
+}
 
+const newArtist = async (req, res) => {
+  try {
+    const { artist } = req.body
+    const response = await database.artist.create({
+      data: {
+        name: artist
+      }
+    })
+    return res.status(201).json({ response })
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+}
 
-export { randomArtist };
+export { randomArtist, newArtist }
--- api/src/routes/artist.routes.js
+++ api/src/routes/artist.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { randomArtist } from '../controller/artist.controller.js'
+import { randomArtist, newArtist } from '../controller/artist.controller.js'
 
 const artistRouter = Router()
 
 artistRouter.get('/artist', randomArtist)
+artistRouter.post('/artist', newArtist)
 
 export { artistRouter }
