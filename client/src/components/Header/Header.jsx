ca9d1c54e4e0403967fc96ec153a4ab400367b01
Lucy Lima Santos <lucyatmosphere@gmail.com>
2024-09-16 09:24:58 -0300

    feat: name changes and click events in progress

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,20 +1,24 @@
 import "boxicons";
 import './Header.css'
 function Header() {
+  const handleClick = () => {
+    
+  }
+
   return (
     <header className="header">
-      <h2 className="header-logo">Random Music</h2>
+      <h2 className="header-logo">RMUS</h2>
       <nav className="header-nav">
         <ul className="header-nav-ul">
-          <li className="header-nav-ul-li">
+          <li className="header-nav-ul-li" onClick={handleClick}>
             <box-icon name="music" type="solid"></box-icon>
             Song
           </li>
-          <li className="header-nav-ul-li">
+          <li className="header-nav-ul-li" onClick={handleClick}>
             <box-icon name="user" type="solid"></box-icon>
             Artist
           </li>
-          <li className="header-nav-ul-li">
+          <li className="header-nav-ul-li" onClick={handleClick}>
             <box-icon name="album" type="solid"></box-icon>
             Album
           </li>
